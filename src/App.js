71afc20cd770e9c23d2e40d7d8d78b8255bd289e
vervarun <veervarun05@gmail.com>
2019-11-04 07:51:55 +0530

    first commit for call api using fetch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import spinner from './spinner.gif';
 import './App.css';
+export default class App extends Component {
+  state = {
+    isLoading: false,
+    items: []
+  };
+  /*comment*/
+  componentDidMount() {
+    fetch('http://www.mocky.io/v2/56fcf6871000000e13aed252')
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          isLoading: true,
+          items: res
+        });
+      });
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    const { isLoading, items } = this.state;
+    if (!isLoading) {
+      return (
+        <div>
+          <img className="spin-img" src={spinner} alt="Data is loading" />
+        </div>
+      );
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.id}>
+              {item.title}|| {item.creation_date}||{item.description}
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  }
 }
-
-export default App;
